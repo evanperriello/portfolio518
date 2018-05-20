d322282fca9e58f06de516f42968537f8267d0f0
Evan Perriello <evan.perriello@gmail.com>
2018-05-20 10:47:07 -0400

    refactor slider into class for reusability

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,42 +1,81 @@
 (function(){
-        let webSlideCount = 0;
-        let writeSlideCount = 0;
-        const slides = document.querySelectorAll("#web-projects .project");
-
-            function slide(arrow, counter){
-                const nextSlide = slides[webSlideCount + 1] || slides[0];
-                const prevSlide = slides[webSlideCount - 1] || slides[slides.length - 1];
-                const currSlide = slides[webSlideCount];
-
-                if (arrow.dataset.direction === 'next'){
-                    currSlide.classList.remove('project--shown');
-                    currSlide.classList.add('project--left');
-                    nextSlide.classList.add('project--right');
-                    setTimeout(()=>{
-                        currSlide.classList.remove('project--left');
-                        nextSlide.classList.remove('project--right');
-                        nextSlide.classList.add('project--shown');
-                    }, 500);
-                    webSlideCount = (webSlideCount == slides.length - 1) ? 0 : webSlideCount + 1;
-                } else {
-                    currSlide.classList.remove('project--shown');
-                    currSlide.classList.add('project--right');
-                    prevSlide.classList.add('project--left')
-                    setTimeout(()=>{
-                        currSlide.classList.remove('project--right');
-                        prevSlide.classList.remove('project--left');
-                        prevSlide.classList.add('project--shown');
-                }, 500);
-                webSlideCount = (webSlideCount == 0) ? slides.length -1 : webSlideCount - 1;
-                }
+        class Slider {
+            constructor(args){
+                this.count = args.initialCount;
+                this.slides = args.slides;
+                this.name = args.name;
+                this.time = args.time;
+                this.noClick = false;
             }
-        document.getElementById('web-next').addEventListener(
-            'click',
-            (e)=>{return slide(e.target, webSlideCount)}
-        );
-        document.getElementById('web-prev').addEventListener(
-            'click',
-            (e)=>{return slide(e.target, webSlideCount)}
-        );
+            slide(arrow){
+                console.log('sliding');
+                const slides = this.slides;
+                const nextSlide = slides[this.count + 1] || slides[0];
+                const prevSlide = slides[this.count - 1] || slides[slides.length - 1];
+                const currSlide = slides[this.count];
+                //the noClick check makes sure the user can't click again before the sliding finishes and stack up a bunch of slides.
+                if (!this.noClick){
+                    if (arrow.dataset.direction === 'next'){
+                        this.noClick = true;
+                        currSlide.classList.remove('project--shown');
+                        currSlide.classList.add('project--left');
+                        nextSlide.classList.add('project--right');
+                        setTimeout(()=>{
+                            currSlide.classList.remove('project--left');
+                            nextSlide.classList.remove('project--right');
+                            nextSlide.classList.add('project--shown');
+                            this.noClick = false;
+                        }, this.time);
+                        this.count = (this.count == slides.length - 1) ? 0 : this.count + 1;
+                    } else {
+                        this.noClick = true;
+                        currSlide.classList.remove('project--shown');
+                        currSlide.classList.add('project--right');
+                        prevSlide.classList.add('project--left')
+                        setTimeout(()=>{
+                            currSlide.classList.remove('project--right');
+                            prevSlide.classList.remove('project--left');
+                            prevSlide.classList.add('project--shown');
+                            this.noClick = false;
+                    }, this.time);
+                    this.count = (this.count == 0) ? slides.length -1 : this.count - 1;
+                    }
+                }
+
+                
+            }
+            //attach it to the dom events
+            attach(){
+                document.getElementById(`${this.name}-next`).addEventListener(
+                    'click',
+                    (e)=>{return webSlider.slide(e.target, "web")}
+                )
+                document.getElementById(`${this.name}-prev`).addEventListener(
+                    'click',
+                    (e)=>{return webSlider.slide(e.target, "web")}
+                );
+
+            }
+        }
+
+        const webSlider = new Slider({
+            name: 'web', 
+            initialCount: 0, 
+            time: 300, 
+            slides: document.querySelectorAll("#web-projects .project")
+        });
+        webSlider.attach();
+
+        const writingSlider = new Slider({
+            name: 'writing',
+            initialCount: 0, 
+            time: 100, 
+            slides: document.querySelectorAll("#write-projects .project")
+        });
+        writingSlider.attach();
+
+
+
     }
+
 )();
